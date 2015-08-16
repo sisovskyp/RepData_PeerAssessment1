b4c25126fee4566f169f75ac93a3bfc229930655
Peter Sisovsky <sisovskyp@gmail.com>
2015-08-16 22:45:00 +0200

    changes
    changes to project 1

--- project_1.R
+++ project_1.R
@@ -10,17 +10,19 @@ str(activitydata)
 
 totalnumber <- data.frame(tapply(activitydata$steps, activitydata$date, sum))
 colnames(totalnumber) <- c("steps total")
+png(file = "plot1.png")
 hist(totalnumber$`steps total`, breaks = 100, main = "Histogram of total number of steps per day", xlab = "total number of steps")
+dev.off()
 
 ## mean per day
 
 mean <- data.frame(tapply(activitydata$steps, activitydata$date, mean))
 colnames(mean) <- c("steps mean")
-barplot(mean$`steps mean`)
+png(file = "plot2.png")
+barplot(mean$`steps mean`, xlab = "date", ylab = "mean of the number of steps", main = "Mean of the total number of steps per day")
+dev.off()
 
 ## median per day
-median(activitydata$steps, na.rm = FALSE)
-
 median <- data.frame(tapply(activitydata$steps, activitydata$date, FUN = (median, na.rm = FALSE)))
 colnames(median) <- c("steps median")
 barplot(median$`steps median`)
